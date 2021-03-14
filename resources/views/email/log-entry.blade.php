870651419ea931e298f231b6cb83d3666c276fe0
Johan Santosa <johanekasantosa@yahoo.com>
2021-03-14 17:01:16 +0700

    Fix Entry Log

--- resources/views/email/log-entry.blade.php
+++ resources/views/email/log-entry.blade.php
@@ -2,22 +2,19 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <title>New Log Entry - {{ $log->name }}</title>
 </head>
 
 <body>
   <h4>Hi, {{ $name }}!</h4>
   <p>There's a new log added with details:</p>
-  <div style="margin: 20rem;">
+  <div>
     <p>Log ID: {{ $id }}</p>
     <p>Log Title: {{ $log->title }}</p>
     <p>Log Detail: {{ $log->description }}</p>
   </div>
   <p>You can review or update this later on the app!</p>
-  <p style="margin-top: 20rem; margin-bottom: 20rem;">Enjoy your day!</p>
+  <p>Enjoy your day!</p>
 </body>
 
 </html>
